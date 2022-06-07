1bf6d82934563622d79a77e85082c771af3d59ac
Daniel Gordillo <dgordilloguillen@gmail.com>
2022-06-07 09:12:14 +0200

    Desarrollando Interfaz

---
+++ src/iArchivoConfig.php
@@ -0,0 +1,13 @@
+<?php
+namespace ITEC\PRESENCIAL\DAW\PROG\iArchivoConfig; 
+interface iArchivoConfig {
+    public function modifyFile();
+    public function modifyFile();
+    public function modifyFile();
+    public function modifyFile();
+    public function modifyFile();
+    public function modifyFile();
+}
+
+
+?>
